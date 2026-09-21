0353bcb7dca999a04ab455d4fd48dbc791bc5d75
Daniel Ohtra <ohtradaniel@gmail.com>
2026-09-21 13:46:10 +0300

    add js and css

--- index.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs'
-
-let tekst = fs.readFileSync("index.js")
-
-console.log(tekst)
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import math from './math.js';
+
+let answear = math.add(4,5)
+
+console.log(answear)
